9066d3eee2a7a334df0e5984638e9c770ceb4613
MyListon <myliston@gmail.com>
2025-11-28 16:36:42 -0300

    :sparkles:Ajustes App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
@@ -14,29 +13,6 @@ function App() {
     <Sobregrupo />
     <Sobreprojeto />
     </>
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-
-import Home from "./pages/home/Home";
-import ListaClientes from "./components/clientes/listaclientes/ListaClientes";
-import ListaOportunidades from "./components/oportunidades/ListaOportunidades/ListaOportunidades";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="min-h-[80vh] p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/clientes" element={<ListaClientes />} />
-          <Route path="/oportunidades" element={<ListaOportunidades />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
->>>>>>> feature/components-01
   );
 }
 
