4bf2323db86c9ed7361da792027d1d425c26ef9c
Luilully <vicarialuiza@gmail.com>
2025-11-28 15:19:07 -0300

    :tada: criação de Footer e NavBar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,21 @@
 function Navbar() {
     return (
         <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    CRMFy
 
+                    <div className='flex gap-4'>
+                        Clientes
+                        Oportunidades
+                        Equipe
+                        Projeto
+                        Sair
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
